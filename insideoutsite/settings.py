c98344c1df6b071600de9d03ba02a34c72bb01c5
AMDevG <jeberry308@gmail.com>
2016-01-11 18:09:55 -0600

    Changd Root URL

--- insideoutsite/settings.py
+++ insideoutsite/settings.py
@@ -52,7 +52,7 @@ MIDDLEWARE_CLASSES = (
     
 )
 
-ROOT_URLCONF = 'mysite.urls'
+ROOT_URLCONF = 'insideoutsite.urls'
 
 TEMPLATES = [
     {
@@ -70,7 +70,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'mysite.wsgi.application'
+WSGI_APPLICATION = 'insideoutsite.wsgi.application'
 
 
 # Database
